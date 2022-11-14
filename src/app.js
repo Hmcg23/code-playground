ae773c711ea2bf0b3a11eaa67602fe3e03698586
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-11-13 22:31:07 -0800

    small edits

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ window.onload = () => {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext("2d");
     const mainRed = '#ae3a3a';
+    /*
     // line 1
     context.moveTo(0, 0);
     context.lineTo(600, 400);
@@ -12,6 +13,7 @@ window.onload = () => {
     context.lineTo(600, 0);
     context.stroke();
     // rectangle
+    */
     /*
     context.beginPath();
     context.rect(250, 150, 100, 100);
@@ -25,17 +27,17 @@ window.onload = () => {
     context.fill();
     */
     // text
-    const text = 'My cat is such a chonker';
+    const text = 'Very very cool';
     context.font = '30px Shrikhand';
     context.lineWidth = 10;
-    context.strokeStyle = 'blue'
-    context.fillText(text, (115), 100);
+    context.fillStyle = mainRed;
+    context.fillText(text, 170, 200);
     // -------------------------------------------------------------------------------------
     // Animations
-    let x = 800;
+    let x = 300;
     let y = 0;
     const draw = () => {
-        context.clearRect(0, 0, 600, 400);
+        context.clearRect(0, 0, 600, 110);
         context.beginPath();
         context.rect(x, y, 100, 100);
         context.lineWidth = 10;
@@ -49,7 +51,7 @@ window.onload = () => {
 
         x += (100 - x) / 10;
         if (x < 101) {
-            x = 800;
+            x = 600;
         }
     }
 
